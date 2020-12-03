4f5775c8ff4ef879f049818b1523533d8c90d5af
Natalya Myunster <natalyamyunster@yahoo.com>
2020-12-03 15:24:16 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,260 @@
+//task1
+// var a = -3;
+//
+// if (a == 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task2
+// var b = 7;
+//
+// if (b > 0) {
+// alert(true);
+// } else {
+// alert(false);
+// }
+//
+// //task3
+// var c = 7;
+//
+// if (c < 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task4
+// var d = 7;
+//
+// if (d >= 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task5
+// var e = 7;
+//
+// if (e <= 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task6
+// var f = 7;
+//
+// if (f != 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task7
+// var g = 'test';
+//
+// if (g == 'test') {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task8
+// var h = '1';
+//
+// if (h === '1') {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task9
+// var test = true;
+//
+// if (test == true) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+//
+// //task10
+// var j = false;
+//
+// if (j != true) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+//task11
+// var k = 2;
+//
+// if (k > 0 && k < 5) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+//task12
+// var l = 0;
+//
+// if (l == 0 || l == 2) {
+//     alert(l + 7);
+// } else {
+//     alert(l / 10);
+// }
+
+//task13
+// var m = 0;
+// var n = 3;
+//
+// if (m <= 1 && n >= 3) {
+//     alert(m + n);
+// } else {
+//     alert(m - n);
+// }
+
+//task14
+// var o = 5;
+// var p = 7;
+//
+// if (o > 2 && o < 11 || p >= 6 && p < 14) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+//task15
+// var seasons = 1;
+// switch (seasons) {
+//     case 1:
+//         alert('winter');
+//         break;
+//     case 2:
+//         alert('spring');
+//         break;
+//     case 3:
+//         alert('summer');
+//         break;
+//     case 4:
+//         alert('autumn');
+//         break;
+//     default:
+//         alert('Error');
+//         break;
+//}
+
+//task16
+// var day = 50;
+//
+// if (day <= 10 && day >= 1 ) {
+//     alert('first');
+//  } else if (day <= 20 && day > 10) {
+//     alert('second');
+// } else if ( day <= 31 && day > 20) {
+//     alert('third')
+// } else alert('error');
+
+// task17
+// var month = 12;
+//
+// if (month <= 3 && month >= 1 ) {
+//     alert('winter');
+//  } else if (month <= 6 && month > 3) {
+//     alert('spring');
+// } else if ( month <= 9 && month > 6) {
+//     alert('summer')
+// } else if ( month <= 12 && month > 9) {
+//     alert('autumn')
+// } else alert('error');
+
+
+//task18
+// var a = 'abcde';
+//
+// if ( a[0] === 'a') {
+//      alert('yes');
+//  } else {
+//      alert('no');
+//  }
+
+//task19
+// var a = '12345';
+//
+// if ( a[0] === '1') {
+//      alert('yes');
+//  } else {
+//      alert('no');
+//  }
+
+//task20
+
+var string = '123';
+var a = Number(string[0]);
+var b = Number(string[1]);
+var c = Number(string[2]);
+var sum = a + b + c;
+alert(sum);
+
+//task21
+
+// var string = '123456';
+//
+// var a = Number(string[0]);
+// var b = Number(string[1]);
+// var c = Number(string[2]);
+// var d = Number(string[3]);
+// var e = Number(string[4]);
+// var f = Number(string[5]);
+//
+// if (a + b + c === d + e + f) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+//task 1
+
+// var txt = 'Hello world!';
+// var x = txt.length;
+// alert(x);
+
+//task2
+
+// var txt = "i can eat bananas all day";
+// var x = txt.slice (11,17);
+// alert(x);
+
+//task 3
+
+// var str1 = "hello";
+// var str2 = "world!";
+// var str3 = str1.concat (" ", str2);
+// alert (str3);
+
+//task 4
+
+// var txt = "abvgeezhziyklmnoprstufkhtschshshch'yueyuya";
+// var pos = txt.charAt (4);
+// alert(pos);
+
+//task 5
+
+// var txt = "Hello World!";
+// txt = txt.replace  ("Hello", "Welcome");
+// alert(txt);
+
+//task 6
+
+// var txt = "Hello World";
+// txt = txt.toUpperCase ();
+// alert(txt);
+
+//task 7
+
+// var txt = "Hello World";
+// txt = txt.toLowerCase ();
+// alert(txt);
+
